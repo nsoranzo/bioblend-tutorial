579c43714cd504624c46e41c2b1ae407ffa9ff8b
Nicola Soranzo <nicola.soranzo@tgac.ac.uk>
2015-07-06 09:25:34 +0100

    Add IPython notebook installation to start_tutorial.sh

--- start_tutorial.sh
+++ start_tutorial.sh
@@ -6,4 +6,5 @@ test -z "$VIRTUAL_ENV" || deactivate
 virtualenv .venv && . .venv/bin/activate
 python -c 'import sys; sys.version_info >= (2, 7, 9) or sys.exit(1)' || pip install --upgrade --force-reinstall requests[security]
 pip install --upgrade bioblend
+pip install ipython[all]
 ipython notebook
