b76839c9416a7434d614bb58ddc1fb95b788b3b5
John Chilton <jmchilton@gmail.com>
2015-11-01 11:23:25 +0000

    Improvements to start_tutorial.sh script.
     - Add ``set -e`` to catch problems right away.
     - Allow targetting bioblend master.

--- start_tutorial.sh
+++ start_tutorial.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+# Stop on problems.
+set -e
+
 cd `dirname $0`
 
-test -z "$VIRTUAL_ENV" || deactivate
+# Target git master using BIOBLEND_TARGET=git+git://github.com/galaxyproject/bioblend.git@master start_tutorial.sh
+BIOBLEND_TARGET="${BIOBLEND_TARGET:-bioblend}"
+
+test -z "$VIRTUAL_ENV" || deactivate || true
 virtualenv .venv && . .venv/bin/activate
 python -c 'import sys; sys.version_info >= (2, 7, 9) or sys.exit(1)' || pip install --upgrade --force-reinstall requests[security]
-pip install --upgrade bioblend
+pip install --upgrade "$BIOBLEND_TARGET"
 pip install ipython[all]
 ipython notebook
