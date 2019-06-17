b5c7bd716927d173ca54206d4232a4439ab5932a
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2019-06-17 21:41:38 +0100

    Fix script for Conda Python

--- start_tutorial.sh
+++ start_tutorial.sh
@@ -3,13 +3,27 @@
 # Stop on problems.
 set -e
 
-cd `dirname $0`
+cd "$(dirname "$0")"
 
 # Target git master using BIOBLEND_TARGET=git+git://github.com/galaxyproject/bioblend.git@master start_tutorial.sh
 BIOBLEND_TARGET="${BIOBLEND_TARGET:-bioblend}"
 
 test -z "$VIRTUAL_ENV" || deactivate || true
 if [ ! -d .venv ]; then
+    # If python is from Conda, make sure virtualenv comes from conda-forge
+    if python -V 2>&1 | grep -q -e 'Anaconda' -e 'Continuum Analytics' || \
+            python -c 'import sys; print(sys.version.replace("\n", " "))' | grep -q -e 'packaged by conda-forge' ; then
+        conda install --yes --channel conda-forge 'virtualenv>=16'
+    fi
+
+    if ! command -v virtualenv >/dev/null; then
+        echo "virtualenv not found, please install it."
+        echo "For Debian/Ubuntu, this can be accomplished with:"
+        echo ""
+        echo "    sudo apt install virtualenv"
+        exit 1
+    fi
+
     virtualenv .venv
 fi
 . .venv/bin/activate
