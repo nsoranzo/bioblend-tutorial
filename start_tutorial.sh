61af4e2d135048f5b4ab488279d6e1eaae3f15b4
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2019-07-17 17:10:24 +0100

    Use ``requirements.txt`` for ``start_tutorial.sh``

--- start_tutorial.sh
+++ start_tutorial.sh
@@ -5,9 +5,6 @@ set -e
 
 cd "$(dirname "$0")"
 
-# Target git master using BIOBLEND_TARGET=git+git://github.com/galaxyproject/bioblend.git@master start_tutorial.sh
-BIOBLEND_TARGET="${BIOBLEND_TARGET:-bioblend}"
-
 test -z "$VIRTUAL_ENV" || deactivate || true
 if [ ! -d .venv ]; then
     # If python is from Conda, make sure virtualenv comes from conda-forge
@@ -28,6 +25,6 @@ if [ ! -d .venv ]; then
 fi
 . .venv/bin/activate
 python -c 'import sys; sys.version_info >= (2, 7, 9) or sys.exit(1)' || pip install --upgrade --force-reinstall requests[security]
-pip install --upgrade "$BIOBLEND_TARGET"
+pip install --upgrade -r requirements.txt
 pip install jupyter
 jupyter notebook
